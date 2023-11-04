4b878263c55639391f916fca45492430a7465d7b
liushengkai <949919743@qq.com>
2023-11-04 17:54:39 +0800

    feat: component s-icon

---
+++ packages/icon/src/icon.vue
@@ -0,0 +1,21 @@
+<template>
+  <i :class="`s-icon-${name}`" :style="{ fontSize: size + 'px' }"></i>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'SIcon',
+  props: {
+    name: {
+      type: String,
+      default: () => '',
+    },
+    size: {
+      type: [String, Number],
+      default: () => '24',
+    },
+  },
+})
+</script>
