0929812e4e0157d6793a74ecc7671611c182a175
liushengkai <949919743@qq.com>
2023-11-06 01:00:16 +0800

    feat: button module

--- packages/button/src/button.vue
+++ packages/button/src/button.vue
@@ -1,10 +1,39 @@
 <template>
-  <button>按钮</button>
+  <button class="s-button" :class="classs">
+    <i v-if="props.icon" :class="`s-icon-${props.icon}`"></i>
+    <span>
+      <slot></slot>
+    </span>
+  </button>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { PropType, defineComponent, computed } from 'vue'
+
+type ButtonType = 'primary' | 'warning' | 'info' | 'danger'
+
 export default defineComponent({
   name: 'SButton',
+  props: {
+    type: {
+      type: String as PropType<ButtonType>,
+      default: () => '',
+    },
+    disabled: Boolean,
+    icon: {
+      type: String,
+      default: () => '',
+    },
+  },
+  setup(props, ctx) {
+    const classs = computed(() => [
+      props.type ? 's-button--' + props.type : '',
+      {
+        'is-disabled': props.disabled,
+      },
+    ])
+    
+    return { classs, props }
+  },
 })
 </script>
